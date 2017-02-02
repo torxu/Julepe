a30c115e2b0c7675496a29b5ceb67464493b8144
torxu <aitoroterocarpintero@gmail.com>
2017-02-02 13:28:01 +0100

    Añadido metodos: Añadir carta, saber baza de la primera carta y carta que va ganando

--- Baza.java
+++ Baza.java
@@ -1,10 +1,46 @@
+import java.util.*;
 public class Baza
 {
-    
-
+    private int numeroJugadoresQueTiran;
+    private int paloQuePinta;
+    private ArrayList<Carta> baza;
     
     public Baza(int numeroJugadoresQueTiran, int paloQuePinta)
     {
-        
+        numeroJugadoresQueTiran = 0;
+        baza = new ArrayList<Carta>();
     }
+    
+    public void addCarta(Carta cartaTirada, String nombreJugador)
+    {
+        baza.add(cartaTirada);
+    }
+    
+    public int getPaloPrimeraCartaDeLaBaza()
+    {
+        int primeraBaza = -1;        
+        if(baza.size() > 0){
+           primeraBaza = baza.get(0).getPalo();
+           paloQuePinta = primeraBaza;
+        }
+        return primeraBaza;
+    }
+    
+    public Carta cartaQueVaGanandoLaBaza()
+    {
+        Carta cartaGanando = null;
+        int comparada = 1;
+        if(!baza.isEmpty()){
+            cartaGanando = baza.get(comparada);
+            for(Carta carta : baza){
+                if(carta.ganaA(cartaGanando, paloQuePinta)){
+                        cartaGanando = carta;
+                        comparada++;
+                }
+            }
+        }
+        return cartaGanando;
+    }
+    
+    
 }
